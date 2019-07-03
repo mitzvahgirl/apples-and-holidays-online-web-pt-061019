59b59a92e7125da7a016285da337fc1bfe6cdddd
Amy Fogelman <abfnj1@gmail.com>
2019-07-03 01:58:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ all all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.map do
+  holiday_hash.map {||}
 end
 
 
