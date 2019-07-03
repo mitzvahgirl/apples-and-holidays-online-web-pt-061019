4b4cdfe5ed978c9fbd8355d403057d85dd0e8ef1
Amy Fogelman <abfnj1@gmail.com>
2019-07-03 00:44:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   def add_supply_to_winter_holidays(holiday_hash, item)
  holiday_hash[:winter].each do |holiday, supplies|
+   supplies >> supply
   end
 end
 
